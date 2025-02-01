aff815075df94a4af7dca5124a1e1b4dfd03a02e
pilons <rincha.boncha1124@icloud.com>
2025-02-01 16:49:56 +0900

    Revert "[feat]add_login_feature"
    This reverts commit e7c206227f8cb34e0fb06f1c6171d4bc06ff14bc.

--- devices/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Device
-
-
-class DeviceForm(forms.ModelForm):
-    class Meta:
-        model = Device
-        fields = ['name', 'ip_address', 'device_type', 'status', 'location', 'notes']
